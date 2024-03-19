535cfe40802f5e088ef8b083232bd134078b491f
LucasLLimeira <Lucasdllimeira@hotmail.com>
2024-03-19 17:56:16 -0300

    Alteração

--- Teste/src/Main.java
+++ Teste/src/Main.java
@@ -13,7 +13,7 @@ public class Main {
         for (int i = 0; i < 3; i++)
             menorColuna[i] = 10;
 
-        //maior elemento na linha 0
+        //maior elemento nas linhas
 
 
         for (int j = 0; j < 3; j++) {
