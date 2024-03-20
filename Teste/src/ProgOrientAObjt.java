4fb9455e6082205c1c141fbc6e6d20058b572ee0
LucasLLimeira <Lucasdllimeira@hotmail.com>
2024-03-19 22:21:29 -0300

    Update 3

---
+++ Teste/src/ProgOrientAObjt.java
@@ -0,0 +1,12 @@
+public class ProgOrientAObjt {
+    public static void main(String[] args){
+
+        Cao cachorro = new Cao();
+        cachorro.Anda();
+
+        Cao dog = new Cao("Lulu", 12);
+        dog.DadosCao();
+
+    }
+
+}
